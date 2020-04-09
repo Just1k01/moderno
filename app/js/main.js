198002ba3b6f8309d506c14d5eb34585e5f2a16e
Just1k01 <ljustfunkl@inbox.ru>
2020-04-09 21:54:07 +0300

    added product-slider and product

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,19 @@
 $(function () {
 
+  $(".rate-star").rateYo({
+    normalFill: "#A0A0A0",
+    starWidth: "12px",
+    rating: 5,
+    readOnly: true
+  });
+  $('.product__slider-inner').slick({
+    arrows: false,
+    dots: true,
+    slidesToShow: 4,
+    slidesToScroll: 4
+
+  });
+
   var mixer = mixitup('.products__inner-box');
 
 
